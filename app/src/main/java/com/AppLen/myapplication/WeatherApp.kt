0e70c83a5000cfa6ccfaca110951e285e874f7c7
Hanun Quran <hanunhfzhhn@gmail.com>
2023-11-29 10:42:56 +0700

    Commit

--- app/src/main/java/com/AppLen/myapplication/WeatherApp.kt
+++ app/src/main/java/com/AppLen/myapplication/WeatherApp.kt
@@ -1,3 +1,21 @@
 package com.AppLen.myapplication
 
-data class WeatherApp()
+import android.opengl.Visibility
+import java.net.IDN
+import java.util.TimeZone
+
+data class WeatherApp(
+    val base: String,
+    val clouds: Clouds,
+    val cod: Int,
+    val coord: Coord,
+    val dt: Int,
+    val id: Int,
+    val main: Main,
+    val name: String,
+    val sys: Sys,
+    val timeZone: Int,
+    val visibility: Int,
+    val weather: List<Weather>,
+    val wind: Wind
+)
