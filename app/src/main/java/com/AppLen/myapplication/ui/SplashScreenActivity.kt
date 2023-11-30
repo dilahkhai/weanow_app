84d4d58c3c818cc9dceea12aa990fba7a694a896
kiazunnurain <zaskiazunsch@gmail.com>
2023-11-30 09:07:27 +0700

    Updated Project

---
+++ app/src/main/java/com/AppLen/myapplication/ui/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.applen.myapplication.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.AppLen.myapplication.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
