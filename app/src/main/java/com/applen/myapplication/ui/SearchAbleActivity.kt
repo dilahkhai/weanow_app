bee7a34df6b75e10fc740f2efa792c66d8637c67
Hanun Quran <hanunhfzhhn@gmail.com>
2023-11-30 10:40:35 +0700

    Commit

---
+++ app/src/main/java/com/applen/myapplication/ui/SearchAbleActivity.kt
@@ -0,0 +1,12 @@
+package com.applen.myapplication.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.AppLen.myapplication.R
+
+class SearchAbleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_able)
+    }
+}
