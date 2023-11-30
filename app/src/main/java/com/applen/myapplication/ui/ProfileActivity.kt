832e209975d58d0f2608242cb14a40a26dea701a
kiazunnurain <zaskiazunsch@gmail.com>
2023-11-30 14:46:14 +0700

    Updated Project

--- app/src/main/java/com/applen/myapplication/ui/ProfileActivity.kt
+++ app/src/main/java/com/applen/myapplication/ui/ProfileActivity.kt
@@ -3,6 +3,13 @@ package com.applen.myapplication.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.AppLen.myapplication.R
+import com.applen.myapplication.fragment.ApplenFragment
+import com.applen.myapplication.fragment.UsFragment
+import com.applen.myapplication.fragment.WeanowFragment
+
+private val weanowFragment= WeanowFragment()
+private val applenFragment = ApplenFragment()
+private val usFragment = UsFragment()
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
