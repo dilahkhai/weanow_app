c3215e7e7f781ff648a72873440de520f4e76f5b
amelizyn <ameliaa.cahyaniii@gmail.com>
2023-12-01 13:47:54 +0700

    Updated Project

--- app/src/main/java/com/applen/myapplication/ui/ProfileActivity.kt
+++ app/src/main/java/com/applen/myapplication/ui/ProfileActivity.kt
@@ -13,7 +13,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.fragment_weanow)
 
 
         val weanowFragment= WeanowFragment()
