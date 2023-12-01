9ff670b901f09f6c24c7966fba53eb7bf999ecff
kiazunnurain <zaskiazunsch@gmail.com>
2023-12-01 16:31:10 +0700

    Updated Project

---
+++ app/src/main/java/com/applen/myapplication/Item.kt
@@ -0,0 +1,5 @@
+package com.applen.myapplication
+
+data class Item(val name: String){
+
+}
--- app/src/main/java/com/applen/myapplication/ui/ProfileActivity.kt
+++ app/src/main/java/com/applen/myapplication/ui/ProfileActivity.kt
@@ -1,20 +1,30 @@
 package com.applen.myapplication.ui
 
+import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.AppLen.myapplication.R
+import com.AppLen.myapplication.databinding.ActivityProfileBinding
 import com.applen.myapplication.fragment.ApplenFragment
 import com.applen.myapplication.fragment.UsFragment
 import com.applen.myapplication.fragment.WeanowFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+
 
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_weanow)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val profileGroup  = R.id.fab_profile_group
 
         val weanowFragment= WeanowFragment()
         val applenFragment = ApplenFragment()
