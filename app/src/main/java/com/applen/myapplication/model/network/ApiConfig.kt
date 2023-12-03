8f220307c81baca4ad3f8016319a941922897b5d
dilahkhai <dilahkhaizure@gmail.com>
2023-12-03 14:24:53 +0700

    commit

--- app/src/main/java/com/applen/myapplication/model/network/ApiConfig.kt
+++ app/src/main/java/com/applen/myapplication/model/network/ApiConfig.kt
@@ -20,7 +20,7 @@ object ApiConfig {
             .build() //save the detail request
 
         return Retrofit.Builder()
-            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .baseUrl("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}")
             .client(okHttpClient)
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
