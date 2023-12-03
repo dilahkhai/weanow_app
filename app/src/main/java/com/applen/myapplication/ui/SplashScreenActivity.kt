349ebfd73943a466713ebf7bc4fd0b54d861a9f7
amelizyn <ameliaa.cahyaniii@gmail.com>
2023-12-03 11:13:51 +0700

    Updated Project

--- app/src/main/java/com/applen/myapplication/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/applen/myapplication/ui/SplashScreenActivity.kt
@@ -15,15 +15,15 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         // This is used to hide the status bar and make
-        // the splash screen as a full screen activity.
+        // The splash screen as a full screen activity.
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        // we used the postDelayed(Runnable, time) method
-        // to send a message with a delayed time.
-        //Normal Handler is deprecated , so we have to change the code little bit
+        // We used the postDelayed(Runnable, time) method
+        // To send a message with a delayed time.
+        // Normal Handler is deprecated , so we have to change the code little bit
 
         // Handler().postDelayed({
         Handler(Looper.getMainLooper()).postDelayed({
